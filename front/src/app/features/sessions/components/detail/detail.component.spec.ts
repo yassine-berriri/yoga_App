43274fe43a23e14611a9a3d87723e1d9f9efdfb8
I756730 <yassine.berriri@sap.com>
2025-01-03 20:23:51 +0100

    add unit test for details&formComponents

--- front/src/app/features/sessions/components/detail/detail.component.spec.ts
+++ front/src/app/features/sessions/components/detail/detail.component.spec.ts
@@ -1,18 +1,22 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBarModule, MatSnackBar } from '@angular/material/snack-bar';
 import { RouterTestingModule, } from '@angular/router/testing';
 import { expect } from '@jest/globals'; 
 import { SessionService } from '../../../../services/session.service';
 
 import { DetailComponent } from './detail.component';
+import { SessionApiService } from '../../services/session-api.service';
+import { of } from 'rxjs';
 
 
 describe('DetailComponent', () => {
   let component: DetailComponent;
   let fixture: ComponentFixture<DetailComponent>; 
   let service: SessionService;
+  let snackBarMock: jest.Mocked<MatSnackBar>;
+  
 
   const mockSessionService = {
     sessionInformation: {
@@ -21,7 +25,26 @@ describe('DetailComponent', () => {
     }
   }
 
+  const mockSessionApiService = {
+    delete: jest.fn().mockReturnValue(of({})),
+    detail: jest.fn().mockReturnValue(of({
+      id: 1,
+      name: 'Yoga Basics',
+      date: '2024-01-01',
+      description: 'Learn the basics of Yoga.',
+      users: [1, 2]
+    })),
+  };
+
+
+
   beforeEach(async () => {
+
+    snackBarMock = {
+      open: jest.fn(),
+    } as unknown as jest.Mocked<MatSnackBar>;
+
+
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
@@ -30,7 +53,11 @@ describe('DetailComponent', () => {
         ReactiveFormsModule
       ],
       declarations: [DetailComponent], 
-      providers: [{ provide: SessionService, useValue: mockSessionService }],
+      providers: [
+        { provide: MatSnackBar, useValue: snackBarMock },
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: SessionApiService, useValue: mockSessionApiService },
+      ],
     })
       .compileComponents();
       service = TestBed.inject(SessionService);
@@ -42,5 +69,15 @@ describe('DetailComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // test unitaire Suppression d’une session
+  it('should delete a session', () => {
+    component.sessionId = '1';
+    component.delete();
+    
+    expect(snackBarMock.open).toHaveBeenCalledWith('Session deleted !', 'Close',  { duration: 3000 });
+
+  });
+
 });
 
--- front/src/app/features/sessions/components/form/form.component.spec.ts
+++ front/src/app/features/sessions/components/form/form.component.spec.ts
@@ -14,18 +14,37 @@ import { SessionService } from 'src/app/services/session.service';
 import { SessionApiService } from '../../services/session-api.service';
 
 import { FormComponent } from './form.component';
+import { data } from 'cypress/types/jquery';
+import { of } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 describe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
+  let snackBarMock: jest.Mocked<MatSnackBar>;
 
   const mockSessionService = {
     sessionInformation: {
       admin: true
     }
-  } 
+  }
+  
+  const mockSessionApiService = {
+    create: jest.fn().mockReturnValue(of({
+      name: 'Yoga Basics',
+      description: 'Learn the basics of Yoga.',
+      date: '2024-01-01',
+    })),
+  };
 
   beforeEach(async () => {
+
+    snackBarMock = {
+      open: jest.fn(),
+    } as unknown as jest.Mocked<MatSnackBar>;
+
+   
+
     await TestBed.configureTestingModule({
 
       imports: [
@@ -40,9 +59,10 @@ describe('FormComponent', () => {
         MatSelectModule,
         BrowserAnimationsModule
       ],
-      providers: [
+      providers: [ { 
+        provide: MatSnackBar, useValue: snackBarMock },
         { provide: SessionService, useValue: mockSessionService },
-        SessionApiService
+        {provide: SessionApiService, useValue: mockSessionApiService}
       ],
       declarations: [FormComponent]
     })
@@ -56,4 +76,41 @@ describe('FormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  // test unitaire should create session
+  it('should create session', () => {
+    const sessionForm = {name: 'Yoga Basics', date: '2024-01-01', teacher_id: 1,  description: 'Learn the basics of Yoga.',};
+
+    component.sessionForm?.setValue(sessionForm);
+
+    component.submit();
+
+    expect(mockSessionApiService.create).toHaveBeenCalledWith(sessionForm);
+
+    expect(snackBarMock.open).toHaveBeenCalledWith('Session created !', 'Close',  { duration: 3000 });
+
+  });
+
+  // test unitaire display error when form is invalid
+  it ("should display error when form is invalid", () => {
+    component.sessionForm?.setValue({name: '', date: '', teacher_id: '', description: ''});
+    fixture.detectChanges();
+
+    const form = component.sessionForm;
+
+    if (form){
+      expect(form.invalid).toBeTruthy();
+      expect(form.controls['name'].hasError('required')).toBeTruthy();
+      expect(form.controls['date'].hasError('required')).toBeTruthy();
+      expect(form.controls['teacher_id'].hasError('required')).toBeTruthy();
+      expect(form.controls['description'].hasError('required')).toBeTruthy();
+    }
+
+    
+
+  });
+
+
+
 });
